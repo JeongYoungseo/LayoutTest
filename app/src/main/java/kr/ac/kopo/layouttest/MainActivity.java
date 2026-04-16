674e01357baa5fa18ea2c675dfd1bd0bd6538211
JeongYoungseo <wjddudtjjys@gmail.com>
2026-04-16 16:25:52 +0900

    layoutTest

--- app/src/main/java/kr/ac/kopo/layouttest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/layouttest/MainActivity.java
@@ -15,27 +15,58 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear_mint, linear_skyBlue;
+    Button mint, skyBlue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
 
-        LinearLayout linear = new LinearLayout(this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        linear.setOrientation(LinearLayout.VERTICAL);
-        linear.setBackgroundColor(Color.rgb(210,255,230));
-        Button btn = new Button(this);
-        btn.setText("code button");
-        linear.addView(btn);
-        btn.setBackgroundColor(Color.rgb(255,200,210));
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"헬롱",Toast.LENGTH_SHORT).show();
-            }
+        setContentView(R.layout.activity_main12);
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main12), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-        setContentView(linear,params);
+
+        linear_mint = findViewById(R.id.linear_mint);
+        linear_skyBlue = findViewById(R.id.linear_skyBlue);
+        mint = findViewById(R.id.mint);
+        skyBlue = findViewById(R.id.skyBlue);
+
+        mint.setOnClickListener(btnListener);
+        skyBlue.setOnClickListener(btnListener);
+
+
+//        LinearLayout linear = new LinearLayout(this);
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+//        linear.setOrientation(LinearLayout.VERTICAL);
+//        linear.setBackgroundColor(Color.rgb(210,255,230));
+//        Button btn = new Button(this);
+//        btn.setText("code button");
+//        linear.addView(btn);
+//        btn.setBackgroundColor(Color.rgb(255,200,210));
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),"헬롱",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        setContentView(linear,params);
+
 
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            linear_mint.setVisibility(View.INVISIBLE);
+            linear_skyBlue.setVisibility(View.INVISIBLE);
+            Button ebtn = (Button) v;
+            if(ebtn == mint){
+                linear_mint.setVisibility(View.VISIBLE);
+            } else {
+                linear_skyBlue.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 }
